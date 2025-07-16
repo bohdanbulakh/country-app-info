6defaf8cd493491c4024a481ba6f4760f3082297
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-07-16 14:00:19 +0300

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,72 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import unusedImports from "eslint-plugin-unused-imports";
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^type$' }],
+      'semi': ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'arrow-parens': ['error', 'always'],
+      'arrow-spacing': 'error',
+      'block-spacing': 'error',
+      'brace-style': 'error',
+      'comma-spacing': 'error',
+      'func-call-spacing': 'error',
+      'rest-spread-spacing': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'indent': ['error', 2, { 'SwitchCase': 1 }],
+      'key-spacing': 'error',
+      'keyword-spacing': 'error',
+      'space-before-function-paren': 'error',
+      'new-parens': 'error',
+      'quotes': ['error', 'single'],
+      'space-before-blocks': 'error',
+      'space-in-parens': 'error',
+      'eqeqeq': ['error', 'smart'],
+      'unused-imports/no-unused-imports': 'error',
+      'no-trailing-spaces': ['error', { skipBlankLines: false, }],
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+);
