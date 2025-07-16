5230fed742927c5444a2e4972dbd9ba3e5b5c131
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-07-16 17:40:51 +0300

    feature: added endpoint for user creating

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('users')
-export class UserController {}
+export class UserController {
+  constructor (private readonly userService: UserService) {}
+
+  @Post(':userId/calendar/holidays')
+  createUserHolidays (
+    @Param('userId') userId: string,
+  ) {}
+
+  @Post()
+  create (
+    @Body() body: CreateUserDto,
+  ) {
+    return this.userService.create(body);
+  }
+}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../database/prisma.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor (private readonly prisma: PrismaService) {}
+
+  create (data: CreateUserDto) {
+    return this.prisma.user.create({ data });
+  }
+}
