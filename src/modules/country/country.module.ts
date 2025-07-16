1363d68564b09f687cbe3240faae4a7c3f576742
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-07-16 14:56:21 +0300

    enhancement: created country controller and service

--- src/modules/country/country.module.ts
+++ src/modules/country/country.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CountryController } from './country.controller';
+import { CountryService } from './country.service';
 
 @Module({
-  controllers: [],
-  providers: [],
+  controllers: [CountryController],
+  providers: [CountryService],
 })
 export class CountryModule {}
